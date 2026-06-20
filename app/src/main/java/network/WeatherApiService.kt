56f2326b137974a7d4b4873ca7552fe000f09d34
patiencenjeri99 <patiencenjeri.shirandula@my.jcu.edu.au>
2026-06-21 08:31:22 +1000

    Create weather API service and response models

---
+++ app/src/main/java/network/WeatherApiService.kt
@@ -0,0 +1,16 @@
+package network
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+
+    @GET("v1/forecast")
+    suspend fun getWeather(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("current") current: String = "temperature_2m,relative_humidity_2m,apparent_temperature,weather_code",
+        @Query("daily") daily: String = "temperature_2m_max,weather_code",
+        @Query("timezone") timezone: String = "Australia/Brisbane"
+    ): WeatherResponse
+}
---
+++ app/src/main/java/network/WeatherResponse.kt
@@ -0,0 +1,32 @@
+package network
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherResponse(
+    val current: CurrentWeather,
+    val daily: DailyWeather
+)
+
+data class CurrentWeather(
+    @SerializedName("temperature_2m")
+    val temperature: Double,
+
+    @SerializedName("relative_humidity_2m")
+    val humidity: Int,
+
+    @SerializedName("apparent_temperature")
+    val apparentTemperature: Double,
+
+    @SerializedName("weather_code")
+    val weatherCode: Int
+)
+
+data class DailyWeather(
+    val time: List<String>,
+
+    @SerializedName("temperature_2m_max")
+    val maxTemperatures: List<Double>,
+
+    @SerializedName("weather_code")
+    val weatherCodes: List<Int>
+)
